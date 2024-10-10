be04c43817a88d3754444742dc02aa8405cba973
Daniel Wajdowicz <143103970+DanielWajd@users.noreply.github.com>
2024-10-10 07:51:18 +0000

    Zad 2

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 (function () {
-  //TODO
+  document.getElementById("ex2_text").addEventListener("input", function(){
+    const input = this.value;
+    const content = document.getElementById("ex2_content");
+    content.innerHTML = ''; 
+        let regex = /[a-zA-Z]/;
+        let specialChar = /[!@#$%^&*()\-+={}[\]:;"'<>,.?\/|\\]/
+
+      if (input.length !== 9) {
+        content.innerHTML += 'Długość numeru musi być równa 9<br>';
+      }if (regex.test(input)) {
+        content.innerHTML += 'Numer nie może zawierać liter<br>';
+      }if (specialChar.test(input)) {
+        content.innerHTML += 'Numer nie może zawierać znaków specjalnych<br>';
+      }if(input.length ==9){
+        content.innerHTML += " Numer telefonu jets poprawny<br>";
+    }
+  })
 })();
