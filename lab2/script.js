727f8bc10d2ed8d9f9b78bd0348a844173376c1a
Damian Świeca <damianswieca04@gmail.com>
2024-10-24 09:06:45 +0200

    Cm2_1

--- lab2/script.js
+++ lab2/script.js
@@ -5,6 +5,18 @@
   const cw3 = document.getElementById('cw3')
   const answer = document.getElementById('answer')
 
+  function prepareString(array){
+    toInsert = "";
+    toInsert += "<ol>";
+    for (el of array){
+      toInsert += `<li><u>Post nr ${el.id} użytkownika nr ${el.userId}</u><br>`;
+      toInsert += `<b>${el.title}</b><br>`;
+      toInsert += `${el.body}<br><br></li>`;
+    }
+    toInsert += "</ol>"
+    return toInsert;
+  }
+
   example.addEventListener("click", function () {
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.json())
@@ -14,8 +26,14 @@
       })
   })
 
-  cw1.addEventListener("click", function () {
-    //TODO
+  cw1.addEventListener("click", function () { 
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(array => {
+        
+        toInsert = prepareString(array);
+        answer.innerHTML = toInsert;
+      })
   })
 
   cw2.addEventListener("click", function () {
